81945021067284259c55515850e782a7709ac17b
Danish <iamdanishk555@gmail.com>
2022-05-09 21:08:59 +0545

    initial commit

---
+++ app/src/main/java/com/example/simpleapicalldemo/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.simpleapicalldemo
+
+import android.app.Dialog
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.gson.Gson
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.DataOutputStream
+import java.io.IOException
+import java.io.InputStreamReader
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.SocketTimeoutException
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Here we will call the asyncTask class for the api calling and pass the required parameters to it.
+        //CallAPILoginAsyncTask("Panjutorials", "123456").execute()
+    }
+
+
+}
---
+++ app/src/main/java/com/example/simpleapicalldemo/ResponseData.kt
@@ -0,0 +1,8 @@
+package com.example.simpleapicalldemo
+
+/**
+ * A class for creating a data class which will be used to parse the Json response using GSON.
+ */
+data class ResponseData(
+    
+)
